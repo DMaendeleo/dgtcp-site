9f8ab8ceeb328a2810e1d6a51f37c33530819e66
dmaendeleo <dmaendeleo@github.com>
2025-10-13 16:57:22 +0100

    resolve the graphyQL variable names again

--- frontend/src/routes/+page.server.ts
+++ frontend/src/routes/+page.server.ts
@@ -1,4 +1,4 @@
-import {Rapports} from '$lib/types/Rapport';
+import type {Rapports} from '$lib/types/Rapport';
 import type {Annonces} from "$lib/types/Annonce";
 
 export const load = async ({fetch}) => {
@@ -26,15 +26,16 @@ export const load = async ({fetch}) => {
     }
   `;
 
-    const base = "http://localhost:4000/api/graphql"
+
     const mVariables = {
         "page": 1,
         "limit": 12,
         "sort": "-createdAt",
     }
 
-    const newsQuery = `
-    query AnnoncesList( $page: Int, $limit: Int, $sort: String){
+    const newsQuery =
+        `
+       query AnnoncesList( $page: Int, $limit: Int, $sort: String){
        Annonces(page: $page, limit: $limit, sort: $sort) {
           docs {
               id
@@ -90,6 +91,7 @@ export const load = async ({fetch}) => {
 //     // where: year ? { year: { equals: Number(year) } } : undefined
 // }
 
+    const base = process.env.PAYLOAD_DOCKER_GRAPHQL_URL
     const res = await fetch(`${base}`, {
         method: 'POST',
         headers: {'content-type': 'application/json'},
@@ -100,26 +102,54 @@ export const load = async ({fetch}) => {
     const resNews = await fetch(`${base}`, {
         method: 'POST',
         headers: {'content-type': 'application/json'},
-        body: JSON.stringify({newsQuery, nVariables})
+        body: JSON.stringify(
+            { query:
+                `query AnnoncesList( $page: Int, $limit: Int, $sort: String){
+       Annonces(page: $page, limit: $limit, sort: $sort) {
+          docs {
+              id
+              headline
+              subheadline
+              description
+              slug
+              localisation
+              year
+              image {
+                id
+                filename
+                mimeType
+                filesize
+                url
+              }
+              createdAt
+              updatedAt
+          }
+          page
+          totalPages
+          hasNextPage
+          hasPrevPage
+       }
+    }`, variables: nVariables})
     });
 
     const json = await res.json();
     const jsonNews = await resNews.json();
     console.log(json.data)
     console.log(jsonNews.data)
-    console.log(JSON.stringify(json.data, null, 2));
-    console.log(JSON.stringify(jsonNews.data, null, 3));
 
-    // if (json.errors) {
-    //     console.log("found an error")
-    //     console.error(json.errors);
-    //     throw new Error('GraphQL error');
-    // }
+
+    console.log(JSON.stringify(json.data, null, 2));
+    console.log(JSON.stringify(jsonNews, null, 2));
+
+    if (jsonNews.errors) {
+        console.log("found an error")
+        console.error(json.errors);
+        throw new Error('GraphQL error');
+    }
 
     // const { data } = await res.json();
     // return { rapports: data.Rapports };
 
-    return {rapports: json.data as Rapports, news: jsonNews.data as Annonces}
+    return {rapports: json.data as Rapports, news: jsonNews as Annonces}
+    // return {rapports: json.data as Rapports}
 }
-
-// const { data } = await res.json();
